cc252301882070b7cacebb9516cbb83471a76571
Het1819 <sailesh5419@gmail.com>
2025-03-20 16:53:58 +0530

    Water Thirsty

--- hungry.py
+++ hungry.py
@@ -2,5 +2,8 @@ hung = input("Are you Hungry ?")
 
 if hung == "yes":
     print("I am Hungry")
+    print("Eat Mango")
 else:
-    print("Drink Water")
+    thirsty = input("are you Thirsty?")
+    if thirsty =="yes":
+        print("Drink Water")
