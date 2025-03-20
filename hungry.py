c5f5fcbc755db527926f6efd5ddc1b11a233b3a7
Het1819 <sailesh5419@gmail.com>
2025-03-20 16:58:07 +0530

    Pizza Burger fries

--- hungry.py
+++ hungry.py
@@ -2,5 +2,8 @@ hung = input("Are you Hungry ?")
 
 if hung == "yes":
     print("I am Hungry")
+    print("Eat pizza")
+    print("Eat burger")
+    print("Eat fries")
 else:
     print("Drink Water")
